144d6f7e112b2bab64090df37db3187d545271c6
alex <alex@pdp7.net>
2024-02-25 11:12:09 +0000

    Initial add

---
+++ ubpkg/github.py
@@ -0,0 +1,14 @@
+import github
+
+
+def get_releases(gh_repo):
+    releases = github.Github().get_repo(gh_repo).get_releases()
+    return [(r.tag_name, r.created_at) for r in releases]
+
+
+def get_asset_url(gh_repo, release, name):
+    release = github.Github().get_repo(gh_repo).get_release(release)
+    assets = [a for a in release.assets if a.name == name]
+    assert len(assets) == 1, f"len({assets}) != 1 when searching asset {name} in {gh_repo}-{release}"
+    asset = assets[0]
+    return asset.browser_download_url
