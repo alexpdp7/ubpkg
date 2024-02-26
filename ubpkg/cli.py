7209c18ea3e3fe6d3dec11edddc133a5542e74d8
alex <alex@pdp7.net>
2024-02-26 00:48:10 +0000

    Improve help

--- ubpkg/cli.py
+++ ubpkg/cli.py
@@ -7,7 +7,7 @@ from ubpkg import manifests
 def run():
     logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser()
-    parser.add_argument("manifest")
+    parser.add_argument("manifest", help="path to a manifest or name of a package")
     args = parser.parse_args()
     manifest = manifests.Manifest(args.manifest)
     manifest.install()
