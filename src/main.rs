e5bcdaee675151ed0c42bd5bb2fedcb73b4cebd2
alex <alex@pdp7.net>
2024-03-02 16:36:08 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -64,8 +64,31 @@ impl std::fmt::Display for GitHubRepo {
 fn repo_methods(builder: &mut MethodsBuilder) {
     fn latest_release(#[starlark(this)] receiver: Value) -> anyhow::Result<GitHubRelease> {
         let repo = receiver.downcast_ref::<GitHubRepo>().unwrap();
+
+        let refs = std::process::Command::new("git")
+            .arg("ls-remote")
+            .arg("--tags")
+            .arg("--refs")
+            .arg(format!("https://github.com/{}.git", repo.id))
+            .stderr(std::process::Stdio::inherit())
+            .output()
+            .unwrap();
+
+        assert!(refs.status.success());
+
+        let output = String::from_utf8(refs.stdout).unwrap();
+        let mut versions = output
+            .split('\n')
+            .flat_map(|l| l.split_once('\t')) // the last line is blank :(
+            .map(|(_hash, id)| id)
+            .map(|id| id.strip_prefix("refs/tags/").unwrap())
+            .collect::<Vec<&str>>();
+
+        versions.sort_by(|a: &&str, b: &&str| vsort::compare(a, b));
+
         Ok(GitHubRelease {
             github_repo: repo.clone(),
+            tag: versions.last().unwrap().to_string(),
         })
     }
 }
@@ -93,6 +116,7 @@ impl AllocFrozenValue for GitHubRepo {
 #[derive(Debug, ProvidesStaticType, NoSerialize, Allocative, Clone)]
 struct GitHubRelease {
     github_repo: GitHubRepo,
+    tag: String,
 }
 
 impl std::fmt::Display for GitHubRelease {
@@ -105,18 +129,15 @@ impl std::fmt::Display for GitHubRelease {
 fn release_methods(builder: &mut MethodsBuilder) {
     fn name(#[starlark(this)] receiver: Value) -> anyhow::Result<String> {
         let release = receiver.downcast_ref::<GitHubRelease>().unwrap();
-        Ok("foo".to_string())
+        Ok(release.tag.clone())
     }
 
-    fn download_asset(
-        #[starlark(this)] receiver: Value,
-        name: &str,
-    ) -> anyhow::Result<GitHubAsset> {
+    fn get_asset_url(#[starlark(this)] receiver: Value, name: &str) -> anyhow::Result<String> {
         let release = receiver.downcast_ref::<GitHubRelease>().unwrap();
-        Ok(GitHubAsset {
-            github_release: release.clone(),
-            name: name.to_string(),
-        })
+        Ok(format!(
+            "https://github.com/{}/releases/download/{}/{}",
+            release.github_repo.id, release.tag, name,
+        ))
     }
 }
 
@@ -205,7 +226,12 @@ fn base(builder: &mut GlobalsBuilder) {
     fn install_binary(contents: Value, name: &str) -> anyhow::Result<i32> {
         Ok(0)
     }
-    fn extract(asset: Value, archive_format: &str, path: &str) -> anyhow::Result<FileContents> {
+    fn extract_from_url(
+        url: &str,
+        archive_format: &str,
+        path: &str,
+    ) -> anyhow::Result<FileContents> {
+        dbg!(url, archive_format, path);
         Ok(FileContents {})
     }
 }
