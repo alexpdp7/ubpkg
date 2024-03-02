ab27e34c301388afd178a9600b5e859cb7cc041c
alex <alex@pdp7.net>
2024-03-02 11:37:52 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -49,7 +49,7 @@ fn main() {
     println!("{:?}", res);
 }
 
-#[derive(Debug, ProvidesStaticType, NoSerialize, Allocative)]
+#[derive(Debug, ProvidesStaticType, NoSerialize, Allocative, Clone)]
 struct GitHubRepo {
     id: String,
 }
@@ -61,10 +61,12 @@ impl std::fmt::Display for GitHubRepo {
 }
 
 #[starlark_module]
-fn methods(builder: &mut MethodsBuilder) {
-    fn latest_release(#[starlark(this)] receiver: Value) -> anyhow::Result<i32> {
+fn repo_methods(builder: &mut MethodsBuilder) {
+    fn latest_release(#[starlark(this)] receiver: Value) -> anyhow::Result<GitHubRelease> {
         let repo = receiver.downcast_ref::<GitHubRepo>().unwrap();
-        Ok(0)
+        Ok(GitHubRelease {
+            github_repo: repo.clone(),
+        })
     }
 }
 
@@ -72,7 +74,7 @@ fn methods(builder: &mut MethodsBuilder) {
 impl<'v> StarlarkValue<'v> for GitHubRepo {
     fn get_methods() -> Option<&'static Methods> {
         static RES: MethodsStatic = MethodsStatic::new();
-        RES.methods(methods)
+        RES.methods(repo_methods)
     }
 }
 
@@ -83,6 +85,107 @@ impl<'v> AllocValue<'v> for GitHubRepo {
 }
 
 impl AllocFrozenValue for GitHubRepo {
+    fn alloc_frozen_value(self, heap: &FrozenHeap) -> FrozenValue {
+        heap.alloc_simple(self)
+    }
+}
+
+#[derive(Debug, ProvidesStaticType, NoSerialize, Allocative, Clone)]
+struct GitHubRelease {
+    github_repo: GitHubRepo,
+}
+
+impl std::fmt::Display for GitHubRelease {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "gh:{}", self.github_repo)
+    }
+}
+
+#[starlark_module]
+fn release_methods(builder: &mut MethodsBuilder) {
+    fn name(#[starlark(this)] receiver: Value) -> anyhow::Result<String> {
+        let release = receiver.downcast_ref::<GitHubRelease>().unwrap();
+        Ok("foo".to_string())
+    }
+
+    fn download_asset(
+        #[starlark(this)] receiver: Value,
+        name: &str,
+    ) -> anyhow::Result<GitHubAsset> {
+        let release = receiver.downcast_ref::<GitHubRelease>().unwrap();
+        Ok(GitHubAsset {
+            github_release: release.clone(),
+            name: name.to_string(),
+        })
+    }
+}
+
+#[starlark_value(type = "github_release")]
+impl<'v> StarlarkValue<'v> for GitHubRelease {
+    fn get_methods() -> Option<&'static Methods> {
+        static RES: MethodsStatic = MethodsStatic::new();
+        RES.methods(release_methods)
+    }
+}
+
+impl<'v> AllocValue<'v> for GitHubRelease {
+    fn alloc_value(self, heap: &'v Heap) -> Value<'v> {
+        heap.alloc_simple(self)
+    }
+}
+
+impl AllocFrozenValue for GitHubRelease {
+    fn alloc_frozen_value(self, heap: &FrozenHeap) -> FrozenValue {
+        heap.alloc_simple(self)
+    }
+}
+
+#[derive(Debug, ProvidesStaticType, NoSerialize, Allocative)]
+struct GitHubAsset {
+    github_release: GitHubRelease,
+    name: String,
+}
+
+impl std::fmt::Display for GitHubAsset {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "release:{} name:{}", self.github_release, self.name)
+    }
+}
+
+#[starlark_value(type = "github_release")]
+impl<'v> StarlarkValue<'v> for GitHubAsset {}
+
+impl<'v> AllocValue<'v> for GitHubAsset {
+    fn alloc_value(self, heap: &'v Heap) -> Value<'v> {
+        heap.alloc_simple(self)
+    }
+}
+
+impl AllocFrozenValue for GitHubAsset {
+    fn alloc_frozen_value(self, heap: &FrozenHeap) -> FrozenValue {
+        heap.alloc_simple(self)
+    }
+}
+
+#[derive(Debug, ProvidesStaticType, NoSerialize, Allocative)]
+struct FileContents {}
+
+impl std::fmt::Display for FileContents {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "filecontents")
+    }
+}
+
+#[starlark_value(type = "file_contents")]
+impl<'v> StarlarkValue<'v> for FileContents {}
+
+impl<'v> AllocValue<'v> for FileContents {
+    fn alloc_value(self, heap: &'v Heap) -> Value<'v> {
+        heap.alloc_simple(self)
+    }
+}
+
+impl AllocFrozenValue for FileContents {
     fn alloc_frozen_value(self, heap: &FrozenHeap) -> FrozenValue {
         heap.alloc_simple(self)
     }
@@ -99,10 +202,10 @@ fn github(builder: &mut GlobalsBuilder) {
 
 #[starlark_module]
 fn base(builder: &mut GlobalsBuilder) {
-    fn install_binary(a: &str, b: &str, c: &str) -> anyhow::Result<i32> {
+    fn install_binary(contents: Value, name: &str) -> anyhow::Result<i32> {
         Ok(0)
     }
-    fn extract(a: &str, b: &str) -> anyhow::Result<i32> {
-        Ok(0)
+    fn extract(asset: Value, archive_format: &str, path: &str) -> anyhow::Result<FileContents> {
+        Ok(FileContents {})
     }
 }
