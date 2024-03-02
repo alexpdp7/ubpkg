cac6d411a7494b3004a1c0fa1b081c3d647c0c16
alex <alex@pdp7.net>
2024-03-02 17:55:57 +0100

    First version that does anything

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ use starlark::values::FrozenValue;
 use starlark::values::Heap;
 use starlark::values::ProvidesStaticType;
 use starlark::values::StarlarkValue;
+use std::os::unix::fs::PermissionsExt;
 
 const REPO: Dir = include_dir!("repo");
 
@@ -189,7 +190,9 @@ impl AllocFrozenValue for GitHubAsset {
 }
 
 #[derive(Debug, ProvidesStaticType, NoSerialize, Allocative)]
-struct FileContents {}
+struct FileContents {
+    contents: Vec<u8>,
+}
 
 impl std::fmt::Display for FileContents {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -224,14 +227,46 @@ fn github(builder: &mut GlobalsBuilder) {
 #[starlark_module]
 fn base(builder: &mut GlobalsBuilder) {
     fn install_binary(contents: Value, name: &str) -> anyhow::Result<i32> {
+        let contents = contents.downcast_ref::<FileContents>().unwrap();
+        let mut dest = directories::UserDirs::new()
+            .unwrap()
+            .home_dir()
+            .to_path_buf();
+        dest.push(".local");
+        dest.push("bin");
+        dest.push(name);
+        let dest = dest.as_path();
+        std::fs::write(dest, contents.contents.clone()).unwrap();
+        let mut perms = std::fs::metadata(dest).unwrap().permissions();
+        perms.set_mode(perms.mode() | 0o100);
+        std::fs::set_permissions(dest, perms).unwrap();
         Ok(0)
     }
-    fn extract_from_url(
-        url: &str,
-        archive_format: &str,
-        path: &str,
-    ) -> anyhow::Result<FileContents> {
-        dbg!(url, archive_format, path);
-        Ok(FileContents {})
+    fn extract_from_url(url: &str, path: &str) -> anyhow::Result<FileContents> {
+        let path = path.to_string();
+        let tmp_dir = tempdir::TempDir::new("ubpkg").unwrap();
+        let mut extracted_path = tmp_dir.path().to_path_buf();
+        extracted_path.push(path.clone());
+        let mut asset_path = tmp_dir.path().to_path_buf();
+        asset_path.push(url.split('/').last().unwrap());
+        std::fs::write(
+            asset_path.clone(),
+            &reqwest::blocking::get(url).unwrap().bytes().unwrap(),
+        )
+        .unwrap();
+        let x = extracted_path.clone();
+        decompress::decompress(
+            asset_path,
+            tmp_dir.path().to_path_buf(),
+            &decompress::ExtractOptsBuilder::default()
+                .filter(move |p| p == x)
+                .build()
+                .unwrap(),
+        )
+        .unwrap();
+
+        Ok(FileContents {
+            contents: std::fs::read(dbg!(extracted_path)).unwrap(),
+        })
     }
 }
