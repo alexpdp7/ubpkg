22b8e91d292bafe80d57fd69daa7ad75b088e5d6
alex <alex@pdp7.net>
2024-03-04 11:46:26 +0100

    Only set executable permissions on non-Windows

--- src/base.rs
+++ src/base.rs
@@ -6,7 +6,6 @@ use starlark::starlark_simple_value;
 use starlark::values::{
     starlark_value, NoSerialize, ProvidesStaticType, StarlarkValue, Value, ValueLike,
 };
-use std::os::unix::fs::PermissionsExt;
 
 #[derive(Debug, ProvidesStaticType, NoSerialize, Allocative)]
 struct FileContents {
@@ -36,9 +35,14 @@ pub fn base(builder: &mut GlobalsBuilder) {
         dest.push(name);
         let dest = dest.as_path();
         std::fs::write(dest, contents.contents.clone())?;
-        let mut perms = std::fs::metadata(dest).unwrap().permissions();
-        perms.set_mode(perms.mode() | 0o100);
-        std::fs::set_permissions(dest, perms).unwrap();
+        #[cfg(not(windows))]
+        {
+            use std::os::unix::fs::PermissionsExt;
+
+            let mut perms = std::fs::metadata(dest).unwrap().permissions();
+            perms.set_mode(perms.mode() | 0o100);
+            std::fs::set_permissions(dest, perms).unwrap();
+        }
         Ok(0)
     }
 
